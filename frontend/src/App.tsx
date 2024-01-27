88dc1da0371e6a57f9733e4d355703084bc5ebf9
Yusuf Nissar <yusufnissar8@gmail.com>
2024-01-27 18:38:46 -0500

    frontend stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Chat from "./pages/Chat";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Login></Login>}></Route>
+          <Route path="/chat" element={<Chat></Chat>}></Route>
         </Routes>
       </div>
     </Router>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Gender, ItextMessage } from "../types";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
 interface IUserInputs {
@@ -23,33 +24,47 @@ function Login() {
   const onSubmit: SubmitHandler<IUserInputs> = (data: IUserInputs) =>
     console.log(data);
 
+  let navigate = useNavigate();
+
   return (
     <div className="body">
       <p className="loginTitle">User Information</p>
       <form className="userLogin" onSubmit={handleSubmit(onSubmit)}>
         <input
-          defaultValue={"First Name"}
-          {...register("age", { required: true })}
+          className="inputField"
+          placeholder={"First Name"}
+          {...register("firstName", { required: true })}
         />
         <input
-          defaultValue={"Last Name"}
+          className="inputField"
+          placeholder={"Last Name"}
+          {...register("lastName", { required: true })}
+        />
+        <input
+          className="inputField"
+          placeholder={"Age"}
           {...register("age", { required: true })}
         />
-        <input defaultValue={"Age"} {...register("age", { required: true })} />
-        <input
-          defaultValue={"Gender"}
+        <select
+          className="inputField"
           {...register("gender", { required: true })}
-        />
+        >
+          <option value={"Male"}>Male</option>
+          <option value={"Male"}>Female</option>
+          <option value={"Male"}>Other</option>
+        </select>
         <input
-          defaultValue={"Describe a past situation you want to understand"}
+          className="inputField large"
+          placeholder={"Describe a past situation you want to understand"}
           {...register("situationalInformation", { required: true })}
         />
         <input
-          defaultValue={"What major events happened around this time?"}
+          className="inputField large"
+          placeholder={"What major events happened around this time?"}
           {...register("majorEventsDescription", { required: true })}
         />
         <input type="file" {...register("image", { required: true })} />
-        <input type="submit"></input>
+        <input type="submit" onClick={() => navigate("/chat")}></input>
       </form>
     </div>
   );
